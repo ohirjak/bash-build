bf7a70a933bbbfaa3a74a9e4ce63a9ecde780e6a
Ondrej Hirjak <ohirjak@gmail.com>
2017-09-07 17:54:32 +0200

    Add “build” of packages

--- build.sh
+++ build.sh
@@ -7,3 +7,9 @@ mkdir target
 cp src/uw.sh target/uw-$VERSION.sh
 
 sed -i '' "s/@@@VERSION@@@/$VERSION/g" target/uw-$VERSION.sh
+
+if [ -d "src/packages" ] ; then
+    mkdir -p target/packages-$VERSION
+
+    cp -R src/packages/* target/packages-0.11-SNAPSHOT/
+fi
